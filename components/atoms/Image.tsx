b98443c739c49f0f7e5a6fd6d249083d5e2ec670
tera_ny <haru25haru02@gmail.com>
2022-01-26 22:24:09 +0900

    fix projects

--- components/atoms/Image.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-const Image = styled.img`
-    width: ${props => props.width};
-    max-Width: 700px;
-    min-width: 200px
-
-`
-export default Image
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,9 @@
 import * as React from "react";
-import Link from "next/link";
-import json from "~/metaData.json";
+
 export default () => {
-  const metaDatas: { [key: string]: any } = json;
   return (
-    <React.Fragment>
-      <h1>記事一覧</h1>
-      <div>
-        {Object.entries(metaDatas).map(([key, value]) => {
-          return (
-            <React.Fragment>
-              <Link href={key}>
-                <a>{value.title}</a>
-              </Link>
-              <br />
-            </React.Fragment>
-          );
-        })}
-      </div>
-    </React.Fragment>
+    <>
+      <h2>teranyan</h2>
+    </>
   );
 };
