d96ac95d09059e75dd6c696f862844dd411fe9ac
tera_ny <haru25haru02@gmail.com>
2022-05-08 14:04:26 +0900

    add apple music player

--- pages/fave.tsx
+++ pages/fave.tsx
@@ -24,6 +24,20 @@ const Page: NextPage = () => {
           >
           </iframe>
         </div>
+        <iframe
+          allow="autoplay *; encrypted-media *;"
+          frameBorder="0"
+          height="450"
+          style={{
+            width: "100%",
+            maxWidth: "560px",
+            overflow: "hidden",
+            background: "transparent",
+          }}
+          sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+          src="https://embed.music.apple.com/profile/teranyanhttps://embed.music.apple.com/jp/playlist/%E3%83%95%E3%82%A7%E3%82%A4%E3%83%90%E3%83%AA%E3%83%83%E3%83%88-%E3%83%9F%E3%83%83%E3%82%AF%E3%82%B9/pl.pm-d5779e520ff52d7fd4c4c4a054b0e761"
+        >
+        </iframe>
       </main>
       <style jsx>
         {`
